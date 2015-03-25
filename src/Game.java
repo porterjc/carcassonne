683808aba43c1f94e825068c65d3ab45a55e949c
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-03-25 16:12:34 -0400

    Added GameTest and game

---
+++ src/Game.java
@@ -0,0 +1,5 @@
+/**
+ * Created by johnsoaa on 3/25/2015.
+ */
+public class Game {
+}
---
+++ src/GameTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by johnsoaa on 3/25/2015.
+ */
+public class GameTest {
+
+}
