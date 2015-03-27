ed4a4e14a49026b5e10155d4ea6d6ed848593c97
porterjc <porterjc@rose-hulman.edu>
2015-03-27 00:00:47 -0400

    Added basic code to create a functional graphic button

--- src/GraphicButton.java
+++ src/GraphicButton.java
@@ -1,5 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 /**
  * Created by porterjc on 3/26/2015.
  */
-public class GraphicButton {
+public abstract class GraphicButton extends JPanel implements MouseListener{
+
+    public GraphicButton(){
+        super();
+        this.setBackground(Color.BLUE);
+        this.setSize(200, 200);
+    }
+
+    @Override
+    public abstract void mouseClicked(MouseEvent e);
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //does nothing
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //does nothing
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //does nothing
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //does nothing
+    }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by porterjc on 3/26/2015.
@@ -9,13 +10,25 @@ public class MainFrame extends JFrame {
     public MainFrame() {
         super();
         GraphicsDevice graphD = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        graphD.setFullScreenWindow(this);
+        //graphD.setFullScreenWindow(this);
+        this.setSize(800, 600);
+        this.setLayout(new BorderLayout());
         setupScreen();
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     private void setupScreen(){
+        QuitButton quitButton = new QuitButton();
+        this.add(quitButton, BorderLayout.SOUTH);
+    }
 
+    private class QuitButton extends GraphicButton {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            System.exit(0);
+        }
     }
 
 }
