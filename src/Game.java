5045f96cbae22047127ddee1fa6d8beefdcb9968
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-03-26 20:01:12 -0400

    Added testCase for a GameBoard construction
    Since GameBoard was made a class, this automatically passes

--- src/Game.java
+++ src/Game.java
@@ -2,4 +2,5 @@
  * Created by johnsoaa on 3/25/2015.
  */
 public class Game {
+    
 }
--- src/GameBoardTest.java
+++ src/GameBoardTest.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -5,4 +7,9 @@ import static org.junit.Assert.*;
  */
 public class GameBoardTest {
 
+    @Test
+    public void constructorTest(){
+        GameBoard board = new GameBoard();
+        assert board != null : "board not null";
+    }
 }
