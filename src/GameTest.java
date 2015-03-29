d9c7a7b54690af250f4a9d87db357789cea5282a
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-03-29 18:42:32 -0400

    Updated to Assert.assertEquals test 1

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,6 +1,5 @@
-import org.jetbrains.annotations.NotNull;
+import com.sun.istack.internal.NotNull;
 import org.junit.Test;
-
 
 import java.util.Stack;
 
@@ -10,54 +9,54 @@ import static org.junit.Assert.assertEquals;
  * Created by johnsoaa on 3/25/2015.
  */
 public class GameTest {
-    public final int numberOfTiles =72;
+    public final int numberOfTiles = 72;
+
     @Test
-    public void testCreateGame(){
+    public void testCreateGame() {
         Game board = new Game();
+        assertEquals(true, board != null );
+        assertEquals(0, board.getNumberOfTilesLeft() );
+        assertEquals(true, board.getPlayers() != null );
+    }
 
-        assert board != null : "board not null";
-        assert board.getNumberOfTilesLeft() == 0 :" Tiles";
-        assert board.getPlayers() != null : "List of players is not null";
-
-  }
 
     @Test
-    public void testDrawTile(){
+    public void testDrawTile() {
         Game game = makeNewGame();
-        assert game.getNumberOfTilesLeft() == numberOfTiles :" Tiles";
+        assert game.getNumberOfTilesLeft() == numberOfTiles : " Tiles";
         game.drawTile();
-        assert game.getNumberOfTilesLeft() == numberOfTiles - 1:" Tiles";
+        assert game.getNumberOfTilesLeft() == numberOfTiles - 1 : " Tiles";
         game.drawTile();
         game.drawTile();
-        assert game.getNumberOfTilesLeft() == numberOfTiles - 3 :" Tiles";
+        assert game.getNumberOfTilesLeft() == numberOfTiles - 3 : " Tiles";
     }
 
     @NotNull
     private Game makeNewGame() {
         Stack<Tile> testStack = new Stack<Tile>();
-        for(int i =0;i<numberOfTiles;i++)
-        {
+        for (int i = 0; i < numberOfTiles; i++) {
             testStack.push(new Tile());
         }
         return new Game(testStack);
     }
 
     @Test
-    public void testEmptyTileList(){
+    public void testEmptyTileList() {
         Game game = makeNewGame();
-        for(int i =0; i<numberOfTiles;i++){
+        for (int i = 0; i < numberOfTiles; i++) {
             game.drawTile();
         }
-        assert game.getNumberOfTilesLeft() == 0 :" Tiles";
-        assert game.drawTile() == false :"cannot draw tile from empty stack";
+        assert game.getNumberOfTilesLeft() == 0 : " Tiles";
+        assert game.drawTile() == false : "cannot draw tile from empty stack";
     }
+
     @Test
-    public void testIsGameOver(){
+    public void testIsGameOver() {
         Game game = makeNewGame();
-        assert game.isGameOver() == false: "game not over";
-        for(int i =0; i<numberOfTiles;i++){
+        assert game.isGameOver() == false : "game not over";
+        for (int i = 0; i < numberOfTiles; i++) {
             game.drawTile();
         }
-        assert game.isGameOver() == true: "game is over";
+        assert game.isGameOver() == true : "game is over";
     }
 }
