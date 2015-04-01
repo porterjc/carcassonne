2e034d89ebc64e6c6c1898088eed0fcf1e61ce5f
robinsat <robinsat@rose-hulman.edu>
2015-04-01 01:06:46 -0400

    Added GUI components to AbstractTile

--- src/AbstractTile.java
+++ src/AbstractTile.java
@@ -1,11 +1,16 @@
+import java.awt.*;
+
 /**
  * Created by johnsoaa on 3/27/2015.
  */
 public abstract class AbstractTile {
+    private static final int SIZE = 300;
     private static String oxwner;
 
-    public AbstractTile(){
+    private Rectangle display;
 
+    public AbstractTile(){
+        display = new Rectangle(0, 0, SIZE, SIZE);
     }
     public static String getOxwner() {
         return oxwner;
@@ -18,5 +23,11 @@ public abstract class AbstractTile {
     /**
      * TODO add string owner, color meepleColor, snum feature placedON, Places edge (direction)
      */
+
+    public abstract void draw();
+
+    // Gets the displayable rectangle
+    public Rectangle geDisplay(){ return display; }
+
 
 }
