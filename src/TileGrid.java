a3f73a772cfc8f82d0db3eb9c9f0e0c183fe16f0
robinsat <robinsat@rose-hulman.edu>
2015-04-01 01:47:56 -0400

    TileGrid now properly renders arbitrary set of tiles

--- src/TileGrid.java
+++ src/TileGrid.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by robinsat on 3/31/2015.
@@ -10,17 +11,39 @@ public class TileGrid extends JPanel {
     private int numRows;
     // The number of columns this grid currently has
     private int numCols;
+    // This is the current smallest x value of the grid coordinates
+    private int offsetX;
+    // This is the current smallest y value of the grid coordinates
+    private int offsetY;
+
+    private ArrayList<AbstractTile> tileList;
 
 
-    public TileGrid(){
+    public TileGrid() {
         super();
         this.setBackground(Color.blue);
         numRows = 3;
         numCols = 3;
 
-        for (int i = -1; i <= 1; i++) {
+        offsetX = -1;
+        offsetY = -1;
+        tileList = new ArrayList<AbstractTile>();
 
+        // For testing purposes only
+        for (int i = -1; i < 1; i++) {
+            for (int j = -1; j < 1; j++) {
+                AbstractTile tile = new OpenTile(i, j);
+                tileList.add(tile);
+            }
         }
 
     }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        for (AbstractTile tile : tileList) {
+            tile.draw(g, offsetX, offsetY);
+        }
+    }
+
 }
