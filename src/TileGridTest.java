c00ec2b4cd9e6a9144eedfff4d1ff8b2a86cab07
robinsat <robinsat@rose-hulman.edu>
2015-04-01 13:47:13 -0400

    Added test method to TileGridTest

--- src/TileGridTest.java
+++ src/TileGridTest.java
@@ -6,17 +6,25 @@ import java.awt.*;
  */
 public class TileGridTest extends JFrame{
 
+    TileGrid grid;
+
     public TileGridTest() {
         super();
 
         this.setSize(900, 900);
-        this.add(new TileGrid(), BorderLayout.CENTER);
+        grid = new TileGrid();
+        this.add(grid, BorderLayout.CENTER);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        new TileGridTest();
+        TileGridTest tester = new TileGridTest();
+        tester.testRenderTiles();
+    }
+
+    public void testRenderTiles() {
+        
     }
 
 }
