bd58d94f6bcca5ac743ff61e9f861e1b3166ac94
robinsat <robinsat@rose-hulman.edu>
2015-04-01 01:12:06 -0400

    Added PlayableTile and PlayableTileTest classes

---
+++ src/PlayableTile.java
@@ -0,0 +1,9 @@
+/**
+ * Created by robinsat on 4/1/2015.
+ */
+public class PlayableTile extends AbstractTile {
+    @Override
+    public void draw() {
+
+    }
+}
---
+++ src/PlayableTileTest.java
@@ -0,0 +1,7 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by robinsat on 4/1/2015.
+ */
+public class PlayableTileTest extends TestCase {
+}
