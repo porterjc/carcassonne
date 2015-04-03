6aebef0da3f5d87c01dd240060cdbafd81a0f4fb
robinsat <robinsat@rose-hulman.edu>
2015-04-03 00:59:19 -0400

    Added TileLabel and OpenTileLabel graphical components

---
+++ src/OpenTileLabel.java
@@ -0,0 +1,5 @@
+/**
+ * Created by robinsat on 4/3/2015.
+ */
+public class OpenTileLabel extends TileLabel {
+}
---
+++ src/TileLabel.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+/**
+ * Created by robinsat on 4/3/2015.
+ */
+public class TileLabel extends JLabel {
+}
