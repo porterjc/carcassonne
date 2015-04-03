8679f9ac4cae58aaab877cb2b1381ec1e85a022c
robinsat <robinsat@rose-hulman.edu>
2015-04-03 01:18:54 -0400

    Added code to render an open tile

--- src/OpenTileLabel.java
+++ src/OpenTileLabel.java
@@ -1,5 +1,12 @@
+import java.awt.*;
+
 /**
  * Created by robinsat on 4/3/2015.
  */
 public class OpenTileLabel extends TileLabel {
+
+    public OpenTileLabel(int x, int y) {
+        super(x, y);
+    }
+
 }
--- src/TileLabel.java
+++ src/TileLabel.java
@@ -1,7 +1,27 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 /**
  * Created by robinsat on 4/3/2015.
  */
 public class TileLabel extends JLabel {
+
+    // Stores a tile that this graphical object represents
+    private AbstractTile tile;
+
+    // The size in pixels that these labels should be rendered
+    private static final int TILE_PIXEL_SIZE = 300;
+
+    // The coordinates of this tile
+    private int x;
+    private int y;
+
+    //Constructor
+    public TileLabel(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.setBackground(Color.WHITE);
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
 }
