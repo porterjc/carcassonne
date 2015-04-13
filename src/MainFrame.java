49e169ea46b4001596ba8b9bbbb3cedda1f84bb7
robinsat <robinsat@rose-hulman.edu>
2015-04-12 22:32:56 -0400

    Updated main GUI to show a main menu

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 
 /**
  * Created by porterjc on 3/26/2015.
+ * This is the main frame that will appear and render all GUI components
  */
 public class MainFrame extends JFrame {
 
@@ -25,9 +26,16 @@ public class MainFrame extends JFrame {
     private BufferedImage pic;
     private ArrayList<PlayableTile> tiles;
 
+    // Some graphic constants
     private final int SCREEN_WIDTH = 800;
     private final int SCREEN_HEIGHT = 600;
     private final int COMPONENT_MARGIN = 20;
+
+    private final int buttonWidth = 400;
+    private final int buttonHeight = 50;
+
+    private final int smallMargin = 15;
+    private final int largeMargin = 60;
 
     public MainFrame() {
         super();
@@ -37,35 +45,91 @@ public class MainFrame extends JFrame {
         GraphicsDevice graphD = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         graphD.setFullScreenWindow(this);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setupMainPanel();
         setupMainMenu();
-        setupGameOptions();
+        //setupGamePlay();
         setVisible(true);
     }
 
-    private void setupMainMenu() {
-        JPanel backPanel = new JPanel();
-        backPanel.setBackground(new Color(39, 40, 49));
+    private void setupMainPanel() {
+
+        JPanel backPanel;
+        try {
+            BufferedImage background = ImageIO.read(new File("images/background.png"));
+            backPanel = new TiledImagePanel(background);
+        } catch (IOException e) {
+            e.printStackTrace();
+            backPanel = new JPanel();
+            backPanel.setBackground(new Color(39, 40, 49));
+        }
+
         backPanel.setLayout(new BoxLayout(backPanel, BoxLayout.Y_AXIS));
         this.add(backPanel);
-        backPanel.add(Box.createRigidArea(new Dimension(600, 100)));
+
+        try {
+            Image logo = ImageIO.read(new File("images/logo.png"));
+            //Image resizedLogo = logo.getScaledInstance(700, 162, Image.SCALE_DEFAULT);
+            JLabel logoLabel = new JLabel(new ImageIcon(logo));
+            logoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            backPanel.add(logoLabel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         optionsPanel = new JPanel();
         optionsPanel.setBackground(new Color(100, 105, 153));
-        optionsPanel.setMaximumSize(new Dimension(600, 800));
+        optionsPanel.setMaximumSize(new Dimension(600, 600));
+        optionsPanel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
         optionsPanel.setLayout(new BoxLayout(optionsPanel, BoxLayout.Y_AXIS));
         backPanel.add(optionsPanel);
 
     }
 
     /**
+     * Sets up the main menu
+     */
+    private void setupMainMenu() {
+        optionsPanel.removeAll();
+
+        GraphicButton newGameButton = new GraphicButton(buttonWidth, buttonHeight, "New Game") {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                setupGameOptions();
+            }
+        };
+
+        GraphicButton rulesButton = new GraphicButton(buttonWidth, buttonHeight, "Rules") {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //TODO: display rules
+            }
+        };
+
+        GraphicButton quitButton = new GraphicButton(buttonWidth, buttonHeight, "Quit") {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        };
+
+        optionsPanel.add(getMarginArea(largeMargin));
+        optionsPanel.add(newGameButton);
+        optionsPanel.add(getMarginArea(largeMargin));
+        optionsPanel.add(rulesButton);
+        optionsPanel.add(getMarginArea(largeMargin));
+        optionsPanel.add(quitButton);
+
+        revalidate();
+        repaint();
+    }
+
+    /**
      * This sets up the UI to allow a user to set up the options for a game they're going to start
      */
     private void setupGameOptions() {
-        int buttonWidth = 400;
-        int buttonHeight = 50;
-
-        int smallMargin = 15;
-        int largeMargin = 60;
+        optionsPanel.removeAll();
 
         optionsPanel.add(getMarginArea(largeMargin));
 
@@ -84,39 +148,63 @@ public class MainFrame extends JFrame {
         meepleSelectPanel.add(new MeepleButton(Color.BLACK, meepleSelectPanel));
 
         meepleSelectPanel.setSelected(redbutton);
-        redbutton.select();
 
         optionsPanel.add(meepleSelectPanel);
         optionsPanel.add(getMarginArea(largeMargin));
 
         // This lets the user choose how many computer players they want to play against
-        GameLabel playersLabel = new GameLabel("Choose a number of opponents:");
+        GameLabel playersLabel = new GameLabel("Choose number of players:");
         playersLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         optionsPanel.add(playersLabel);
         optionsPanel.add(getMarginArea(smallMargin));
 
         SelectPanel playersSelectPanel = new SelectPanel();
-        SelectableButton button1 = new SelectableButton(50, 50, "1", playersSelectPanel);
+        SelectableButton button1 = new SelectableButton(50, 50, "2", playersSelectPanel);
         playersSelectPanel.add(button1);
-        playersSelectPanel.add(Box.createRigidArea(new Dimension(20, 80)));
-        playersSelectPanel.add(new SelectableButton(50, 50, "2", playersSelectPanel));
         playersSelectPanel.add(Box.createRigidArea(new Dimension(20, 80)));
         playersSelectPanel.add(new SelectableButton(50, 50, "3", playersSelectPanel));
         playersSelectPanel.add(Box.createRigidArea(new Dimension(20, 80)));
         playersSelectPanel.add(new SelectableButton(50, 50, "4", playersSelectPanel));
+        playersSelectPanel.add(Box.createRigidArea(new Dimension(20, 80)));
+        playersSelectPanel.add(new SelectableButton(50, 50, "5", playersSelectPanel));
+        playersSelectPanel.setSelected(button1);
         optionsPanel.add(playersSelectPanel);
+        optionsPanel.add(getMarginArea(largeMargin));
+
+        // This lets the user specify additional rules
+        GameLabel rulesLabel = new GameLabel("Expansions:");
+        rulesLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        optionsPanel.add(rulesLabel);
+        optionsPanel.add(getMarginArea(smallMargin));
+
+        SelectPanel rulesPanel = new SelectPanel();
+        rulesPanel.add(new GraphicCheckBox("River"));
+        rulesPanel.add(Box.createRigidArea(new Dimension(20, 10)));
+        rulesPanel.add(new GraphicCheckBox("Abbot"));
+        optionsPanel.add(rulesPanel);
         optionsPanel.add(getMarginArea(largeMargin));
 
 
         GraphicButton startButton = new GraphicButton(buttonWidth, buttonHeight, "Start Game") {
             @Override
             public void mouseClicked(MouseEvent e) {
-                System.out.println("Button Pressed");
                 setupGamePlay();
             }
         };
-
         optionsPanel.add(startButton);
+        optionsPanel.add(getMarginArea(largeMargin));
+
+        GraphicButton cancelButton = new GraphicButton(buttonWidth, buttonHeight, "Cancel") {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                setupMainMenu();
+            }
+        };
+        optionsPanel.add(cancelButton);
+
+        revalidate();
+        repaint();
+
     }
 
     /**
@@ -127,11 +215,15 @@ public class MainFrame extends JFrame {
         return Box.createRigidArea(new Dimension(400, height));
     }
 
+
+    /**
+     * Starts a new game and sets up the screen to play the game
+     */
     private void setupGamePlay(){
-        this.removeAll();
+       // this.remove(optionsPanel);
         BorderLayout bl = new BorderLayout();
         mainpanel = new JPanel(bl);
-        mainpanel.setBackground(new Color(39, 40, 49));
+        mainpanel.setBackground(new Color(70, 70, 70));
         this.add(mainpanel);
 
         TileGrid tileGrid = new TileGrid();
@@ -188,21 +280,14 @@ public class MainFrame extends JFrame {
 
         //QuitButton quitButton = new QuitButton();
        // mainpanel.add(bottompanel);
+        revalidate();
+        repaint();
     }
 
-    /*
-    private class QuitButton extends GraphicButton {
 
-        public QuitButton(int width, int height) {
-            super(width, height);
-        }
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            System.exit(0);
-        }
-    } */
-
-
+    /**
+     * This class provides a selectable button that holds a color
+     */
     private class MeepleButton extends SelectableButton {
 
         Color color;
@@ -214,6 +299,25 @@ public class MainFrame extends JFrame {
         }
 
 
+    }
+
+    /**
+     * This class provides a panel that tiles an image as a background
+     */
+    private class TiledImagePanel extends JPanel {
+
+        BufferedImage image;
+
+        public TiledImagePanel(BufferedImage image) {
+            this.image = image;
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setPaint(new TexturePaint(image, new Rectangle(400, 400)));
+            g2d.fill(this.getBounds());
+        }
     }
 
 
