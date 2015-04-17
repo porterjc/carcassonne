419f657d4908a222ed46b4844d7d5d6e77e550b0
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-04-17 00:13:01 -0400

    refactored GameTest.java

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,4 +1,6 @@
 import com.sun.istack.internal.NotNull;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.*;
@@ -12,6 +14,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class GameTest {
     public final int numberOfTiles = 72;
+    public ArrayList<Player> players;
 
     @NotNull
     private Game makeNewGame() {
@@ -22,36 +25,46 @@ public class GameTest {
         return new Game(testStack);
     }
 
+    @Before
+    public void setUp() {
+       this.players = new ArrayList<Player>();
+    }
+
+    @After
+    public void cleanUp() {
+        players = null;
+    }
+
     @Test
     public void testCreateGame() {
         Game board = new Game();
         assertEquals(true, board != null);
         assertEquals(0, board.getNumberOfTilesLeft());
-        assertEquals(true, board.getPlayers() != null );
+        assertEquals(true, board.getPlayers() != null);
     }
+
     @Test
     public void testCreateGameWithTwoPlayers() {
         Player p1 = new Player(Color.RED);
         Player p2 = new Player(Color.ORANGE);
-        ArrayList<Player> players = new ArrayList<Player>();
         players.add(p1);
         players.add(p2);
         Game board = new Game(players);
         assertEquals(true, board != null);
         assertEquals(players, board.getPlayers());
     }
+
     @Test
     public void testCreateGameWithThreePlayers() {
         Player p1 = new Player(Color.RED);
         Player p2 = new Player(Color.ORANGE);
         Player p3 = new Player(Color.BLUE);
-        ArrayList<Player> players = new ArrayList<Player>();
-        players.add(p1);
-        players.add(p2);
-        players.add(p3);
-        Game board = new Game(players);
+        this.players.add(p1);
+        this.players.add(p2);
+        this.players.add(p3);
+        Game board = new Game(this.players);
         assertEquals(true, board != null);
-        assertEquals(players, board.getPlayers());
+        assertEquals(this.players, board.getPlayers());
         assertEquals(3, board.getPlayers().size());
     }
 
@@ -59,7 +72,7 @@ public class GameTest {
     @Test
     public void testDrawTile() {
         Game game = makeNewGame();
-        assertEquals(numberOfTiles,game.getNumberOfTilesLeft());
+        assertEquals(numberOfTiles, game.getNumberOfTilesLeft());
         game.drawTile();
         assertEquals(numberOfTiles - 1, game.getNumberOfTilesLeft());
         game.drawTile();
@@ -74,16 +87,16 @@ public class GameTest {
             game.drawTile();
         }
         assertEquals(0, game.getNumberOfTilesLeft());
-        assertEquals(false, game.drawTile() );
+        assertEquals(false, game.drawTile());
     }
 
     @Test
     public void testIsGameOver() {
         Game game = makeNewGame();
-        assertEquals(false, game.isGameOver() );
+        assertEquals(false, game.isGameOver());
         for (int i = 0; i < numberOfTiles; i++) {
             game.drawTile();
         }
-        assertEquals(true, game.isGameOver() );
+        assertEquals(true, game.isGameOver());
     }
 }
