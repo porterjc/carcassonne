237f1f7e1aae22934d27c0e425e20b528caf3792
porterjc <porterjc@rose-hulman.edu>
2015-04-23 19:24:28 -0400

    Modified TestScoreSmallCity and it fails

--- src/PlayableTileTest.java
+++ src/PlayableTileTest.java
@@ -92,9 +92,25 @@ public class PlayableTileTest {
     }
 
     @Test
-    public void testScoreCity() {
+    public void testScoreSmallCity() {
+        HashMap<GlobalVariables.Direction, GlobalVariables.Feature> feature = new HashMap<GlobalVariables.Direction, GlobalVariables.Feature>();
+        feature.put(GlobalVariables.Direction.NORTH, GlobalVariables.Feature.ROAD);
+        feature.put(GlobalVariables.Direction.EAST, GlobalVariables.Feature.ROAD);
+        feature.put(GlobalVariables.Direction.WEST, GlobalVariables.Feature.ROAD);
+        feature.put(GlobalVariables.Direction.SOUTH, GlobalVariables.Feature.CITY);
+        PlayableTile tile1 = new PlayableTile(0, 0, feature);
+
+        HashMap<GlobalVariables.Direction, GlobalVariables.Feature> feature2 = new HashMap<GlobalVariables.Direction, GlobalVariables.Feature>();
+        feature.put(GlobalVariables.Direction.NORTH, GlobalVariables.Feature.CITY);
+        feature.put(GlobalVariables.Direction.EAST, GlobalVariables.Feature.ROAD);
+        feature.put(GlobalVariables.Direction.WEST, GlobalVariables.Feature.ROAD);
+        feature.put(GlobalVariables.Direction.SOUTH, GlobalVariables.Feature.ROAD);
+        PlayableTile tile2 = new PlayableTile(0, 0, feature2);
+
+        tile1.setBottom(tile2);
+
         Set<AbstractTile> alreadyVisited = new HashSet<AbstractTile>();
-        alreadyVisited.add(lastPlaced);
-        assertEquals(0, lastPlaced.scoreCity(alreadyVisited, new HashSet<Meeple>()));
+        alreadyVisited.add(tile1);
+        assertEquals(4, tile1.scoreCity(alreadyVisited, new HashSet<Meeple>()));
     }
 }
