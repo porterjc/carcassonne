a4171457f2dc11d9a348bbb6c10bf0d24d328a34
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-05-01 08:54:03 -0400

    updated scoreLargerCity to reflect change to return value of scoreCity

--- src/PlayableTileTest.java
+++ src/PlayableTileTest.java
@@ -352,7 +352,7 @@ public class PlayableTileTest {
         Set<AbstractTile> alreadyVisited = new HashSet<AbstractTile>();
         Set<GlobalVariables.Direction> directions = new HashSet<GlobalVariables.Direction>();
         directions.add(GlobalVariables.Direction.SOUTH);
-        assertEquals(12, tile1.startScoreCity(alreadyVisited, new HashSet<Meeple>(), directions, true));
+        assertEquals((Integer) 12, tile1.startScoreCity(alreadyVisited, new HashSet<Meeple>(), directions, true).getValue());
     }
 
     @Test
