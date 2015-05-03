8f20281330ab83eb40f9b17377cdc6fdc5a4f37f
porterjc <porterjc@rose-hulman.edu>
2015-05-03 15:29:24 -0400

    Added new enums and removed unused ones.

--- src/GlobalVariables.java
+++ src/GlobalVariables.java
@@ -4,7 +4,7 @@
 public class GlobalVariables {
     public static enum Feature {ROAD, GRASS, RIVER, CITY}
 
-    public static enum Internal {ROAD, GRASS, RIVER, ROADSTOP, MONASTERY, CITY, GARDEN, COATOFARMS}
+    public static enum Internal {GRASS, ROADSTOP, MONASTERY, CITY, GARDEN, COATOFARMS, NSBISECTOR, EWBISECTOR}
 
     public static enum Direction {NORTH, SOUTH, EAST, WEST}
 
