8682c2d7e6262fc755e1f736d6433a87875d8d82
robinsat <robinsat@rose-hulman.edu>
2015-05-06 17:01:29 -0400

    Added PlaceMeepleButtonTest class

---
+++ src/PlaceMeepleButtonTest.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by robinsat on 5/6/2015.
+ */
+public class PlaceMeepleButtonTest extends JFrame {
+
+    public static void main(String[] args) {
+        PlaceMeepleButtonTest testFrame = new PlaceMeepleButtonTest();
+        testFrame.setSize(900, 900);
+        testFrame.setVisible(true);
+    }
+}
