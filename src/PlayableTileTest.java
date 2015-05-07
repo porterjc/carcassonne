191173c435bd21d4577f923d50c8caaa20f5002f
porterjc <porterjc@rose-hulman.edu>
2015-05-07 16:56:41 -0400

    test hasNS/EW when not having bisectors

--- src/PlayableTileTest.java
+++ src/PlayableTileTest.java
@@ -835,6 +835,18 @@ public class PlayableTileTest {
     }
 
     @Test
+    public void testNSbisector0rotationsNoNSbisector(){
+        HashMap<GlobalVariables.Direction, GlobalVariables.Feature> feature = new HashMap<GlobalVariables.Direction, GlobalVariables.Feature>();
+        feature.put(GlobalVariables.Direction.NORTH, GlobalVariables.Feature.ROAD);
+        feature.put(GlobalVariables.Direction.EAST, GlobalVariables.Feature.GRASS);
+        feature.put(GlobalVariables.Direction.WEST, GlobalVariables.Feature.GRASS);
+        feature.put(GlobalVariables.Direction.SOUTH, GlobalVariables.Feature.ROAD);
+
+        PlayableTile tile1 = new PlayableTile(new OpenTile(), new OpenTile(), new OpenTile(), new OpenTile(), feature);
+        assertFalse(tile1.hasNSbisector());
+    }
+
+    @Test
      public void testEWbisector0rotations(){
         Set<GlobalVariables.Internal> internals = new HashSet<GlobalVariables.Internal>();
         internals.add(GlobalVariables.Internal.EWBISECTOR);
@@ -893,6 +905,18 @@ public class PlayableTileTest {
         tile1.rotateTile();
         tile1.rotateTile();
         tile1.rotateTile();
+        assertFalse(tile1.hasEWbisector());
+    }
+
+    @Test
+    public void testEWbisector0rotationsNoEWbisector(){
+        HashMap<GlobalVariables.Direction, GlobalVariables.Feature> feature = new HashMap<GlobalVariables.Direction, GlobalVariables.Feature>();
+        feature.put(GlobalVariables.Direction.NORTH, GlobalVariables.Feature.GRASS);
+        feature.put(GlobalVariables.Direction.EAST, GlobalVariables.Feature.ROAD);
+        feature.put(GlobalVariables.Direction.WEST, GlobalVariables.Feature.ROAD);
+        feature.put(GlobalVariables.Direction.SOUTH, GlobalVariables.Feature.GRASS);
+
+        PlayableTile tile1 = new PlayableTile(new OpenTile(), new OpenTile(), new OpenTile(), new OpenTile(), feature);
         assertFalse(tile1.hasEWbisector());
     }
 }
