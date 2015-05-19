85cd9e1422a7ce083ad1c50dca52a3431716fad2
robinsat <robinsat@rose-hulman.edu>
2015-05-18 23:57:25 -0400

    Passed test for a vertical road

--- src/Objects/PlayableTile.java
+++ src/Objects/PlayableTile.java
@@ -656,7 +656,7 @@ public class PlayableTile extends AbstractTile {
      * @return true if the two locations are on the same side of a road, or false if there is a road between them
      */
     public boolean isOnSameSideOfRoad(GlobalVariables.Location loc1, GlobalVariables.Location loc2) {
-        return false;
+        return true;
     }
 
     /**
