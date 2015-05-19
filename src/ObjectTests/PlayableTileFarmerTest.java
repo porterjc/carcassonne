e77d105b90d8dfb710368f6adc27a358a5ed7786
robinsat <robinsat@rose-hulman.edu>
2015-05-19 02:14:47 -0400

    Added passing test case for 4 way intersection

--- src/ObjectTests/PlayableTileFarmerTest.java
+++ src/ObjectTests/PlayableTileFarmerTest.java
@@ -607,5 +607,24 @@ public class PlayableTileFarmerTest {
         assertTrue(tile1.isOnSameSideOfRoad(GlobalVariables.Location.TOPRIGHT, GlobalVariables.Location.BOTTOMLEFT));
     }
 
+    @Test
+    public void test4WayIntersection() {
+        HashMap<GlobalVariables.Direction, GlobalVariables.Feature> feature1 = new HashMap<>();
+        feature1.put(GlobalVariables.Direction.NORTH, GlobalVariables.Feature.ROAD);
+        feature1.put(GlobalVariables.Direction.EAST, GlobalVariables.Feature.ROAD);
+        feature1.put(GlobalVariables.Direction.WEST, GlobalVariables.Feature.ROAD);
+        feature1.put(GlobalVariables.Direction.SOUTH, GlobalVariables.Feature.ROAD);
+        HashSet<GlobalVariables.Internal> internal1 = new HashSet<>();
+        internal1.add(GlobalVariables.Internal.ROADSTOP);
+        internal1.add(GlobalVariables.Internal.NSBISECTOR);
+        internal1.add(GlobalVariables.Internal.EWBISECTOR);
+        PlayableTile tile1 = new PlayableTile(new OpenTile(), new OpenTile(), new OpenTile(), new OpenTile(), feature1, internal1);
+
+        assertTrue(tile1.isOnSameSideOfRoad(GlobalVariables.Location.BOTTOMRIGHT, GlobalVariables.Location.BOTTOMRIGHT));
+        assertFalse(tile1.isOnSameSideOfRoad(GlobalVariables.Location.BOTTOMRIGHT, GlobalVariables.Location.TOPRIGHT));
+        assertFalse(tile1.isOnSameSideOfRoad(GlobalVariables.Location.BOTTOMRIGHT, GlobalVariables.Location.TOPLEFT));
+        assertFalse(tile1.isOnSameSideOfRoad(GlobalVariables.Location.BOTTOMRIGHT, GlobalVariables.Location.BOTTOMLEFT));
+    }
+
 
 }
