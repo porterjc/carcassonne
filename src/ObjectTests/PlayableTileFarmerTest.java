056961d824bf9bd1b0a044cf31adc060a12c2a16
robinsat <robinsat@rose-hulman.edu>
2015-05-19 02:36:51 -0400

    Added passing test case for left road segment

--- src/ObjectTests/PlayableTileFarmerTest.java
+++ src/ObjectTests/PlayableTileFarmerTest.java
@@ -642,5 +642,22 @@ public class PlayableTileFarmerTest {
         assertTrue(tile1.isOnSameSideOfRoad(GlobalVariables.Location.TOPRIGHT, GlobalVariables.Location.BOTTOMLEFT));
     }
 
+    @Test
+    public void testLeftRoadSegment() {
+        HashMap<GlobalVariables.Direction, GlobalVariables.Feature> feature1 = new HashMap<>();
+        feature1.put(GlobalVariables.Direction.NORTH, GlobalVariables.Feature.GRASS);
+        feature1.put(GlobalVariables.Direction.EAST, GlobalVariables.Feature.GRASS);
+        feature1.put(GlobalVariables.Direction.WEST, GlobalVariables.Feature.ROAD);
+        feature1.put(GlobalVariables.Direction.SOUTH, GlobalVariables.Feature.GRASS);
+        HashSet<GlobalVariables.Internal> internal1 = new HashSet<>();
+        internal1.add(GlobalVariables.Internal.ROADSTOP);
+        internal1.add(GlobalVariables.Internal.MONASTERY);
+        PlayableTile tile1 = new PlayableTile(new OpenTile(), new OpenTile(), new OpenTile(), new OpenTile(), feature1, internal1);
+
+        assertTrue(tile1.isOnSameSideOfRoad(GlobalVariables.Location.TOPLEFT, GlobalVariables.Location.TOPRIGHT));
+        assertTrue(tile1.isOnSameSideOfRoad(GlobalVariables.Location.TOPLEFT, GlobalVariables.Location.BOTTOMLEFT));
+        assertTrue(tile1.isOnSameSideOfRoad(GlobalVariables.Location.TOPLEFT, GlobalVariables.Location.BOTTOMRIGHT));
+    }
+
 
 }
